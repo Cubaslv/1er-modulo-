f8719ceffd9f17aac2e68d384e1cd939940eca54
Cubaslv <cubas_lv@live.com>
2023-07-21 21:10:05 -0500

    UPDATE

---
+++ main.js
@@ -0,0 +1,37 @@
+
+//ALMACENAR DATOS EN VARIABLES
+var nombre = "ABDUL JIMENEZ";
+var altura = 170;
+
+//FIN COMENTARIO
+
+// IMPRIMIR DATOS MANERA SIMPLE
+//  document.write(nombre) ;
+//  document.write(altura) ;
+//FIN DE COMENTARIO
+
+var concatenacion = nombre + " " +  altura;
+
+// IMPRIMIR DATOS 
+var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
+// FIN COMENTARIO
+
+datos.innerHTML += `
+
+<h1> AQUI LA CAJA DE DATOS </H1>
+<h2> Su nombre es: ${nombre} </h2> 
+<h3> Mide: ${altura} </h3>
+
+
+`;
+
+if(altura>= 190 ) {
+
+datos.innerHTML += ` <h1>ERES ALTO BRO </h1>`
+
+}
+else{
+datos.innerHTML += `<h1>ERES BAJO BRO</H1>`    
+}
+
