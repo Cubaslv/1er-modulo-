8878c6b8dae73095e6f7755cac68396c74da8b5c
Cubaslv <cubas_lv@live.com>
2023-07-22 01:59:01 -0500

    UPDATE

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
 //ALMACENAR DATOS EN VARIABLES
 var nombre = "ABDUL JIMENEZ";
 var altura = 170;
@@ -35,3 +35,22 @@ else{
 datos.innerHTML += `<h1>ERES BAJO BRO</H1>`    
 }
 
+for(var i= 2000; i <= 2020; i ++) {
+    // bloque de instruciones
+    datos.innerHTML += "<h2>estamos en el año </h2>"+i;
+}
+*/
+function MUESTRAMINOMBRE (nombre,altura){
+    var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
+    datos.innerHTML += `
+
+<h1> AQUI LA CAJA DE DATOS </H1>
+<h2> Su nombre es: ${nombre} </h2> 
+<h3> Mide: ${altura} </h3>
+
+
+
+`;
+}
+MUESTRAMINOMBRE("SAMSON",165);             
